06579723c65fe6ddc943383da099c2e88c4ea59d
HenriqueVictorino <henriquevictorino@gmail.com>
2016-04-15 14:53:56 -0300

    Organization Review

---
+++ Ara2.AraLabel/Ara2ComponentsServer/Components/AraLabel.cs
@@ -0,0 +1,294 @@
+﻿// Copyright (c) 2010-2016, Rafael Leonel Pontani. All rights reserved.
+// For licensing, see LICENSE.md or http://www.araframework.com.br/license
+// This file is part of AraFramework project details visit http://www.arafrework.com.br
+// AraFramework - Rafael Leonel Pontani, 2016-4-14
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Ara2.Dev;
+
+namespace Ara2.Components
+{
+    [Serializable]
+    [AraDevComponent]
+    public class AraLabel : AraComponentVisualAnchorConteiner, IAraDev
+    {
+        public AraLabel(IAraObject ConteinerFather)
+            : base(AraObjectClienteServer.Create(ConteinerFather, "Div"), ConteinerFather, "AraLabel")
+        {
+            Buttons = new AraLabelButton(this);
+
+            Click = new AraComponentEvent<EventHandler>(this, "Click");
+
+            this.EventInternal += AraLabel_EventInternal;
+            
+            this.MinWidth = 10;
+            this.MinHeight = 17;
+            this.Width = 100;
+            this.Height = 17;
+        }
+
+        public override void LoadJS()
+        {
+            Tick vTick = Tick.GetTick();
+            vTick.Session.AddJs("Ara2/Components/AraLabel/AraLabel.js");
+        }
+
+        private void AraLabel_EventInternal(String vFunction)
+        {
+            switch (vFunction.ToUpper())
+            {
+                case "CLICK":
+                    Click.InvokeEvent(this, new EventArgs());
+                break;
+                case "CLICKBUTTON":
+                    Buttons.RunEventClick(Convert.ToInt32(Tick.GetTick().Page.Request["codbutton"]));
+                break;
+            }
+        }
+
+        public AraComponentEvent<EventHandler> Click;
+        public AraEvent<EventHandler> ClickLink = new AraEvent<EventHandler>();
+        
+        // Eventos Fim
+
+        public void RemoveInterface()
+        {
+            this.TickScriptCall();
+            Tick.GetTick().Script.Send(" vObj.RemoveInterface(); \n");
+        }
+
+        private string _Text="";
+        [AraDevProperty("")]
+        [PropertySupportLayout]
+        public string Text
+        {
+            set
+            {
+                if (this.Childs.Where(a=>!(a is AraDraggable || a is AraResizable || a is AraAnchor)).Count() > 0)
+                    return;
+
+                _Text = value;
+                this.TickScriptCall();
+                Tick.GetTick().Script.Send(" vObj.SetValue('" + AraTools.StringToStringJS(_Text) +  "'); \n");
+
+                if (_Text == "")
+                {
+                    Buttons = new AraLabelButton(this);
+                    if (EventResetButtons.InvokeEvent!=null)
+                        EventResetButtons.InvokeEvent();
+                }
+
+            }
+            get { return _Text; }
+        }
+
+        [AraDevEvent]
+        public AraEvent<Action> EventResetButtons = new AraEvent<Action>();
+
+        public AraLabelButton Buttons;
+
+        private bool _Visible = true;
+        [AraDevProperty(true)]
+        [PropertySupportLayout]
+        public bool Visible
+        {
+            set
+            {
+                _Visible = value;
+                this.TickScriptCall();
+                Tick.GetTick().Script.Send(" vObj.SetVisible(" + (_Visible == true ? "true" : "false") + "); \n");
+            }
+            get { return _Visible; }
+        }
+
+        public void TextAdd(String vNewValue)
+        {
+            this.TickScriptCall();
+            Tick.GetTick().Script.Send(" vObj.TextAdd(\"" + AraTools.StringToStringJS(vNewValue) + "\"); \n");
+            //foreach (string vTmp in SplitByLength(vNewValue,1))
+            //{
+            //    Tick.GetTick().Script.Send(" vObj.TextAdd('" + System.Web.HttpUtility.JavaScriptStringEncode(vTmp) + "'); \n");
+            //}
+        }
+
+        private IEnumerable<string> SplitByLength(string str, int maxLength)
+        {
+            for (int index = 0; index < str.Length; index += maxLength)
+            {
+                yield return str.Substring(index, Math.Min(maxLength, str.Length - index));
+            }
+        }
+
+        public void TextAddEnd()
+        {
+            this.TickScriptCall();
+            Tick.GetTick().Script.Send(" vObj.TextAddEnd(); \n");
+        }
+
+        public enum ESyleOverflow
+        {
+            none,
+            visible,
+            hidden,
+            scroll,
+            auto,
+            inherit
+        }
+
+        private ESyleOverflow _SyleOverflow = ESyleOverflow.none;
+        [AraDevProperty(ESyleOverflow.none)]
+        [PropertySupportLayout]
+        public ESyleOverflow SyleOverflow
+        {
+            get { return _SyleOverflow; }
+            set { 
+                _SyleOverflow = value;
+
+                this.TickScriptCall();
+                Tick.GetTick().Script.Send(" vObj.SetOverflow(\"" + _SyleOverflow.ToString() + "\"); \n");
+            }
+        }
+
+        public void SetCss(string vName, string vValue)
+        {
+            this.TickScriptCall();
+            Tick.GetTick().Script.Send(" vObj.SetCss(\"" + AraTools.StringToStringJS(vName) + "\",\"" + AraTools.StringToStringJS(vValue) + "\"); \n");
+        }
+
+
+        public enum ETextAlign
+        {
+            left,
+            right,
+            center,
+            justify,
+            inherit
+        }
+
+        private ETextAlign _TextAlign = ETextAlign.left;
+        [AraDevProperty(ETextAlign.left)]
+        [PropertySupportLayout]
+        public ETextAlign TextAlign
+        {
+            get { return _TextAlign; }
+            set
+            {
+                _TextAlign = value;
+
+                SetCss("text-align", Enum.GetName(_TextAlign.GetType(), _TextAlign));
+            }
+        }
+
+        public enum ETextVAlign
+        {
+            none,
+            length,
+            baseline,
+            sub,
+            super,
+            top,
+            text_top,
+            middle,
+            bottom,
+            text_bottom,
+            inherit,
+        }
+
+        private ETextVAlign _TextVAlign = ETextVAlign.none;
+        [AraDevProperty(ETextVAlign.none)]
+        [PropertySupportLayout]
+        public ETextVAlign TextVAlign
+        {
+            get { return _TextVAlign; }
+            set
+            {
+                _TextVAlign = value;
+                if (_TextVAlign!= ETextVAlign.none)
+                    _TextVAlignValue = null;
+
+                if (_TextVAlign == ETextVAlign.none)
+                    SetCss("vertical-align", "");
+                else
+                    SetCss("vertical-align", Enum.GetName(_TextVAlign.GetType(), _TextVAlign).Replace("_","-"));
+            }
+        }
+
+        private AraDistance _TextVAlignValue = null;
+        [AraDevProperty]
+        [PropertySupportLayout]
+        public AraDistance TextVAlignValue
+        {
+            get { return _TextVAlignValue; }
+            set
+            {
+                _TextVAlignValue = value;
+                if (_TextVAlignValue!=null)
+                    _TextVAlign = ETextVAlign.none;
+
+                if (_TextVAlignValue == null)
+                    SetCss("vertical-align", "");
+                else
+                    SetCss("vertical-align", "\"" + _TextVAlignValue.ToString() + "\"");
+            }
+        }
+
+        public void Print()
+        {
+            this.TickScriptCall();
+            Tick.GetTick().Script.Send("$('<div>' + vObj.Obj.innerHTML + '</div>').printElement(); \n");
+        }
+
+        #region Ara2Dev
+        private string _Name = "";
+        [AraDevProperty("")]
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        private AraEvent<DStartEditPropertys> _StartEditPropertys = null;
+        public AraEvent<DStartEditPropertys> StartEditPropertys
+        {
+            get
+            {
+                if (_StartEditPropertys == null)
+                {
+                    _StartEditPropertys = new AraEvent<DStartEditPropertys>();
+                    this.Click += this_ClickEdit;
+                }
+
+                return _StartEditPropertys;
+            }
+            set
+            {
+                _StartEditPropertys = value;
+            }
+        }
+        private void this_ClickEdit(object sender, EventArgs e)
+        {
+            if (_StartEditPropertys.InvokeEvent != null)
+                _StartEditPropertys.InvokeEvent(this);
+        }
+
+        private AraEvent<DStartEditPropertys> _ChangeProperty = new AraEvent<DStartEditPropertys>();
+        public AraEvent<DStartEditPropertys> ChangeProperty
+        {
+            get
+            {
+                return _ChangeProperty;
+            }
+            set
+            {
+                _ChangeProperty = value;
+            }
+        }
+        #endregion
+
+    }
+
+}
---
+++ Ara2.AraLabel/Ara2ComponentsServer/Components/AraLabelButton.cs
@@ -0,0 +1,366 @@
+﻿// Copyright (c) 2010-2016, Rafael Leonel Pontani. All rights reserved.
+// For licensing, see LICENSE.md or http://www.araframework.com.br/license
+// This file is part of AraFramework project details visit http://www.arafrework.com.br
+// AraFramework - Rafael Leonel Pontani, 2016-4-14
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Ara2.Components
+{
+    [Serializable]
+    public class AraLabelButton
+    {
+        AraObjectInstance<AraLabel> _Label= new AraObjectInstance<AraLabel>();
+        private AraLabel Label
+        {
+            get { return _Label.Object; }
+            set { _Label.Object = value; }
+        }
+
+        public AraLabelButton(AraLabel vLabel)
+        {
+            Label = vLabel;
+        }
+
+
+
+        private List<CAraLabelButton> AraLabelButtonReturn = new List<CAraLabelButton>();
+        private int AraLabelButtonReturn_New = 1;
+        [Serializable]
+        private class CAraLabelButton
+        {
+            public int Key;
+            public AraEvent<Delegate> Event;
+            public object[] Parametros;
+            public DateTime Date;
+
+            public void InvokeEvent()
+            {
+                Event.InvokeEvent.DynamicInvoke(Parametros);
+            }
+        }
+
+        public enum SStyleButton
+        {
+            Button=1,
+            Link=2
+        }
+
+        public delegate void Click_delegate(AraLabel Object, System.Collections.Hashtable Parans);
+        public string Add(Hashtable Parans, Click_delegate Click)
+        {
+            return Add("", ButtonIco.None, Parans, Click);
+        }
+
+        public string Add(ButtonIco Ico, Hashtable Parans, Click_delegate Click)
+        {
+            return Add("", Ico, Parans, Click);
+        }
+
+        public string Add(string vCaption, Hashtable Parans, Click_delegate Click)
+        {
+            return Add(vCaption, ButtonIco.None, Parans, Click);
+        }
+
+        public string Add(string vCaption, Hashtable Parans, Click_delegate Click,SStyleButton Style)
+        {
+            return Add(vCaption, ButtonIco.None, Parans, Click, Style);
+        }
+        
+        public string Add(string vCaption, ButtonIco Ico, Hashtable Parans, Click_delegate Click)
+        {
+            return Add(vCaption, Ico, Parans, Click,SStyleButton.Button);
+        }
+
+        public string Add(string vCaption, ButtonIco Ico, Hashtable Parans, Click_delegate Click, SStyleButton Style)
+        {
+            return Add("#", vCaption, Ico, Parans, Click, Style);
+        }
+
+        public string Add(string vHref, string vCaption, ButtonIco Ico, Hashtable Parans, Click_delegate Click, SStyleButton Style)
+        {
+            return Add<Click_delegate>(vHref, vCaption, Ico, Style, Click, Label, Parans);
+        }
+
+        public string Add<T>(string vCaption, T vEvent, params object[] vParametros)
+        {
+            return Add<T>(vCaption, ButtonIco.None, vEvent, vParametros);
+        }
+
+        public string Add<T>(string vCaption, ButtonIco Ico, T vEvent, params object[] vParametros)
+        {
+            return Add<T>(vCaption, Ico, SStyleButton.Button, vEvent, vParametros);
+        }
+
+        public string Add<T>(string vCaption, ButtonIco Ico, SStyleButton Style, T vEvent, params object[] vParametros)
+        {
+            return Add<T>("#", vCaption, Ico, Style, vEvent, vParametros);
+        }
+
+        public string Add<T>(string vHref, string vCaption, ButtonIco Ico, SStyleButton Style, T vEvent, params object[] vParametros)
+        {
+            Delegate vRetorno = (Delegate)(object)vEvent;
+
+            int vCodB = AraLabelButtonReturn_New;
+            AraLabelButtonReturn_New++;
+
+            AraEvent<Delegate> Event = new AraEvent<Delegate>();
+            Event += vRetorno;
+
+            AraLabelButtonReturn.Add(new CAraLabelButton()
+            {
+                Key = vCodB,
+                Event = Event,
+                Parametros = vParametros,
+                Date = DateTime.Now
+            });
+
+
+            string vIdB = "AraLabel_Button_" + Label.InstanceID + "_" + vCodB;
+
+            string ScriptIco = "";
+            if (Ico != ButtonIco.None)
+                ScriptIco += "<span class='ui-icon ui-icon-" + Ico.ToString().Replace("_", "-") + " tree-plus '></span>";
+
+            string vReturn = "";
+            if (Style == SStyleButton.Button)
+            {
+                vReturn += "<button id='" + vIdB + "' ";
+                vReturn += " onclick=\"javascript:Ara.GetObject('" + Label.InstanceID + "').OnClickButton('" + vCodB + "');return false;\"";
+                vReturn += " class='ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only' ";
+                vReturn += ">";
+                vReturn += "<span>";
+                vReturn += "<table>";
+                vReturn += "<tr>";
+                if (ScriptIco != "")
+                {
+                    vReturn += "<td>";
+                    vReturn += "<div class='tree-wrap tree-wrap-ltr' style='width: 18px;'>";
+                    vReturn += ScriptIco;
+                    vReturn += "</div>";
+                    vReturn += "</td>";
+                }
+                if (vCaption != "")
+                {
+                    vReturn += "<td>";
+                    vReturn += vCaption;
+                    vReturn += "</td>";
+                }
+                vReturn += "</tr>";
+                vReturn += "</table>";
+                vReturn += "</span>";
+                vReturn += "</button>";
+            }
+            else if (Style == SStyleButton.Link)
+            {
+                vReturn += "<a href='" + vHref + "' onclick=\"javascript:Ara.GetObject('" + Label.InstanceID + "').OnClickButton('" + vCodB + "');return false;\">";
+                vReturn += ScriptIco;
+                vReturn += vCaption;
+                vReturn += "</a>";
+            }
+
+            return vReturn;
+        }
+
+        
+
+        public void RunEventClick(int vCod)
+        {
+            try
+            {
+                AraLabelButtonReturn
+                    .Where(a => a.Key == vCod)
+                    .First()
+                    .InvokeEvent();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Error on click button label.\n" + err.Message);
+            }
+        }
+
+
+        public enum ButtonIco
+        {
+            None,
+            carat_1_n,
+            carat_1_ne,
+            carat_1_e,
+            carat_1_se,
+            carat_1_s,
+            carat_1_sw,
+            carat_1_w,
+            carat_1_nw,
+            carat_2_n_s,
+            carat_2_e_w,
+            triangle_1_n,
+            triangle_1_ne,
+            triangle_1_e,
+            triangle_1_se,
+            triangle_1_s,
+            triangle_1_sw,
+            triangle_1_w,
+            triangle_1_nw,
+            triangle_2_n_s,
+            triangle_2_e_w,
+            arrow_1_n,
+            arrow_1_ne,
+            arrow_1_e,
+            arrow_1_se,
+            arrow_1_s,
+            arrow_1_sw,
+            arrow_1_w,
+            arrow_1_nw,
+            arrow_2_n_s,
+            arrow_2_ne_sw,
+            arrow_2_e_w,
+            arrow_2_se_nw,
+            arrowstop_1_n,
+            arrowstop_1_e,
+            arrowstop_1_s,
+            arrowstop_1_w,
+            arrowthick_1_n,
+            arrowthick_1_ne,
+            arrowthick_1_e,
+            arrowthick_1_se,
+            arrowthick_1_s,
+            arrowthick_1_sw,
+            arrowthick_1_w,
+            arrowthick_1_nw,
+            arrowthick_2_n_s,
+            arrowthick_2_ne_sw,
+            arrowthick_2_e_w,
+            arrowthick_2_se_nw,
+            arrowthickstop_1_n,
+            arrowthickstop_1_e,
+            arrowthickstop_1_s,
+            arrowthickstop_1_w,
+            arrowreturnthick_1_w,
+            arrowreturnthick_1_n,
+            arrowreturnthick_1_e,
+            arrowreturnthick_1_s,
+            arrowreturn_1_w,
+            arrowreturn_1_n,
+            arrowreturn_1_e,
+            arrowreturn_1_s,
+            arrowrefresh_1_w,
+            arrowrefresh_1_n,
+            arrowrefresh_1_e,
+            arrowrefresh_1_s,
+            arrow_4,
+            arrow_4_diag,
+            extlink,
+            newwin,
+            refresh,
+            shuffle,
+            transfer_e_w,
+            transferthick_e_w,
+            folder_collapsed,
+            folder_open,
+            document,
+            document_b,
+            note,
+            mail_closed,
+            mail_open,
+            suitcase,
+            comment,
+            person,
+            print,
+            trash,
+            locked,
+            unlocked,
+            bookmark,
+            tag,
+            home,
+            flag,
+            calendar,
+            cart,
+            pencil,
+            clock,
+            disk,
+            calculator,
+            zoomin,
+            zoomout,
+            search,
+            wrench,
+            gear,
+            heart,
+            star,
+            link,
+            cancel,
+            plus,
+            plusthick,
+            minus,
+            minusthick,
+            close,
+            closethick,
+            key,
+            lightbulb,
+            scissors,
+            clipboard,
+            copy,
+            contact,
+            image,
+            video,
+            script,
+            alert,
+            info,
+            notice,
+            help,
+            check,
+            bullet,
+            radio_off,
+            radio_on,
+            pin_w,
+            pin_s,
+            play,
+            pause,
+            seek_next,
+            seek_prev,
+            seek_end,
+            seek_start,
+            seek_first,
+            stop,
+            eject,
+            volume_off,
+            volume_on,
+            power,
+            signal_diag,
+            signal,
+            battery_0,
+            battery_1,
+            battery_2,
+            battery_3,
+            circle_plus,
+            circle_minus,
+            circle_close,
+            circle_triangle_e,
+            circle_triangle_s,
+            circle_triangle_w,
+            circle_triangle_n,
+            circle_arrow_e,
+            circle_arrow_s,
+            circle_arrow_w,
+            circle_arrow_n,
+            circle_zoomin,
+            circle_zoomout,
+            circle_check,
+            circlesmall_plus,
+            circlesmall_minus,
+            circlesmall_close,
+            squaresmall_plus,
+            squaresmall_minus,
+            squaresmall_close,
+            grip_dotted_vertical,
+            grip_dotted_horizontal,
+            grip_solid_vertical,
+            grip_solid_horizontal,
+            gripsmall_diagonal_se,
+            grip_diagonal_se
+        }
+    }
+
+}
